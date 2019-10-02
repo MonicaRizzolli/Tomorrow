42e9bbc2c094991ef9f3264078a374d4ddbf013d
Felipe Corrêa da Silva Sanches <juca@members.fsf.org>
2019-10-02 15:58:25 -0300

    adding METADATA.pb and enabling some additional fontbakery checks
    (issue #8)

--- build.sh
+++ build.sh
@@ -12,10 +12,10 @@ done
 # Cleanup gftools mess:
 rm output/*-backup-fonttools-prep-gasp.ttf
 
+cp DESCRIPTION.*.html METADATA.pb output
+
 export OPTIONS="--no-progress"
 export OPTIONS="$OPTIONS --exclude-checkid /check/ftxvalidator" # We lack this on Travis.
-export OPTIONS="$OPTIONS --exclude-checkid /check/metadata" # Delete this after creating a METADATA.pb file
-export OPTIONS="$OPTIONS --exclude-checkid /check/description" # & this after a DESCRIPTION.en_us.html file,
 export OPTIONS="$OPTIONS --exclude-checkid /check/varfont" # & this when making a variable font.
 export OPTIONS="$OPTIONS --loglevel INFO"
 fontbakery check-googlefonts $OPTIONS output/*.ttf
